e6bb8fdf59233179671ed8d24139c9a4d99810f0
root <root@sources.sdsoft.net.ru>
2021-09-20 00:18:01 +0300

    Конфиг для тестов

--- environments/dev/common/config/test-local.php
+++ environments/dev/common/config/test-local.php
@@ -3,7 +3,7 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
+            'dsn' => 'pgsql:host=db;dbname=testdb',
         ],
     ],
 ];
